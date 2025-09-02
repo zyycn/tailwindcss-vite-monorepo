a618086cd2f47c7f0dfcf9d11629915573bfd030
Zyy <No Email>
2025-09-02 11:04:57 +0800

    update

--- packages/ui/src/components/navbar/index.vue
+++ packages/ui/src/components/navbar/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="h-20 bg-amber-600 flex items-center px-3">
     <div class="text-2xl">UI Lib Navbar</div>
-    <div class="text-2xl ml-10">Edit this with hmr, not work, but without tailwind is ok, remove styles/index.css @source "../"; see result</div>
+    <div class="text-2xl ml-10">
+      Edit this with hmr, not work, but without tailwind is ok, remove styles/index.css @source "../"; see result
+    </div>
+
+    <div class="test">TEST</div>
   </div>
 </template>
+
+<style scoped>
+/* however edit this work with hmr */
+.test {
+  margin-left: 20px;
+  color: #fff;
+}
+</style>
